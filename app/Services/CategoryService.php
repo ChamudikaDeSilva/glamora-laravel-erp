027d02ef738e7467fa5582acf65e8c0d686d8382
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2025-12-12 17:22:53 +0530

    Add 'is_active' field to Category model and migration; update CategoryService to handle 'is_active' status

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -9,4 +9,15 @@ class CategoryService extends BaseService
     {
         parent::__construct($repository);
     }
+
+    public function create(array $data)
+    {
+        if(isset($data['is_active']))
+        {
+            $data['is_active'] = $data['is_active'] ? 1: 0;
+        }
+        return parent::create($data);
+    }
+
+    
 }
--- database/migrations/2025_10_22_050644_create_categories_table.php
+++ database/migrations/2025_10_22_050644_create_categories_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
-            
+            $table->boolean('is_active')->default(1);
+
             $table->timestamps();
         });
     }
